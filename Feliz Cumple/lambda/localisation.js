c44f57fe850825a14d690e47dca82ead90633e5f
dafierrom <67808000+dafierrom@users.noreply.github.com>
2023-01-04 20:43:05 -0500

    Add files via upload

---
+++ Feliz Cumple/lambda/localisation.js
@@ -0,0 +1,25 @@
+module.exports = {
+    es: {
+        translation: {
+            WELCOME_MSG: 'Te doy la bienvenida a Feliz Cumpleaños. Vamos a divertirnos un poco con tu cumpleaños! ',
+            WELCOME_BACK_MSG: 'Te doy la bienvenida otra vez! ',
+            REGISTER_MSG: 'Your birthday is {{month}} {{day}} {{year}}.',
+            REJECTED_MSG: 'No pasa nada. Por favor dime la fecha otra vez y lo corregimos. ',
+            DAYS_LEFT_MSG: 'Queda {{count}} días para que cumplas {{year}} ',
+            DAYS_LEFT_MSG_plural: 'Quedan {{count}} días ',
+            WILL_TURN_MSG: 'para que cumplas {{count}} año. ',
+            WILL_TURN_MSG_plural: 'para que cumplas {{count}} años. ',
+            GREET_MSG: 'Feliz cumpleaños! Hoy cumples {{count}} año! ',
+            GREET_MSG_plural: 'Feliz cumpleaños! Hoy cumples {{count}} años! ',
+            MISSING_MSG: 'Parece que aun no me has dicho tu fecha de cumpleaños. ',
+            POST_SAY_HELP_MSG: 'Si quieres cambiar la fecha puedes decir, registra mi cumpleaños. O sólo dime la fecha directamente. Qué quieres hacer? ',
+            HELP_MSG: 'Puedo recordar tu cumpleaños si me dices una fecha. O decirte cuanto falta para que cumplas. Qué quieres hacer? ',
+            REPROMPT_MSG: 'Si no sabes como continuar intent pedir ayuda. Si quieres salir solo dí para. Qué quieres hacer? ',
+            GOODBYE_MSG: 'Hasta luego! ',
+            REFLECTOR_MSG: 'Acabas de activar {{intent}}',
+            FALLBACK_MSG: 'Lo siento, no se nada sobre eso. Por favor inténtalo otra vez. ',
+            ERROR_MSG: 'Lo siento, ha habido un problema. Por favor inténtalo otra vez. '
+        }
+    },
+
+}
